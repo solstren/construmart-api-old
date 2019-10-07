badfaeef0ea300551701ad1d9e8c57e339f1da8c
IAmThatOG <gabbygabbylexy@gmail.com>
2019-10-08 00:13:25 +0100

    implemented dynamic port in main.ts and modified start:prod in package.json

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const port = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+  await app.listen(port);
+  Logger.log(`App started at port => ${port}`)
 }
 bootstrap();
