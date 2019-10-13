6bb7d4ee837291c8e44bab55012127e0f5e559e3
Gabriel Ifeanyi <gabriel_ifeanyi@ymail.com>
2019-10-13 09:53:11 +0100

    made modifications to test if heroku deploys review-dev branch

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -16,7 +16,7 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World! Api is live');
+      expect(appController.getHello()).toBe('Hello World!!! Api is up and running');
     });
   });
 });
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! Api is live';
+    return 'Hello World!!! Api is up and running';
   }
 }
