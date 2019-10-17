f9b1809ededb0e6c94d3e27a6781b611a6206d5e
IAmThatG <gabbygabbylexy@gmail.com>
2019-10-17 17:22:37 +0100

    implemented db connection

---
+++ src/config/config.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { ConfigService } from './config.service';
+import { TypeormConfigService } from './typeorm-config.service';
+
+@Module({
+    providers: [
+        {
+            provide: ConfigService,
+            useValue: new ConfigService(`${process.env.NODE_ENV || 'development'}.env`),
+        },
+        TypeormConfigService,
+    ],
+    exports: [ConfigService],
+})
+export class ConfigModule { }
---
+++ src/utils/app-constants.ts
@@ -0,0 +1,4 @@
+export class AppConstants {
+    // env keys
+    
+}
