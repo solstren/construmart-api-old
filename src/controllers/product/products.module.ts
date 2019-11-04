940834953a8b83975cef91e82ec7573c54e6830b
IAmThatG <gabbygabbylexy@gmail.com>
2019-11-04 06:51:40 +0100

    fixed error in product module

--- src/controllers/product/products.module.ts
+++ src/controllers/product/products.module.ts
@@ -3,10 +3,11 @@ import { ProductsController } from './products.controller';
 import { ProductsService } from './services/products.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductsRepository } from './repositories/products.repository';
+import { CategoriesRepository } from '../category/repositories/categories.repository';
 
 @Module({
   controllers: [ProductsController],
-  imports: [TypeOrmModule.forFeature([ProductsRepository])],
+  imports: [TypeOrmModule.forFeature([ProductsRepository, CategoriesRepository])],
   providers: [ProductsService]
 })
 export class ProductModule {}
