69e857182700f107d95b57b54f1f61ab8ebef8a0
IAmThatG <gabbygabbylexy@gmail.com>
2019-11-04 07:41:32 +0100

    added multer module to product module for file uploads

--- src/controllers/product/products.module.ts
+++ src/controllers/product/products.module.ts
@@ -4,10 +4,20 @@ import { ProductsService } from './services/products.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductsRepository } from './repositories/products.repository';
 import { CategoriesRepository } from '../category/repositories/categories.repository';
+import { MulterModule } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { editFileName } from '../../utils/edit-filename';
+import { imageFileValidator } from '../../utils/image-validator';
 
 @Module({
-  controllers: [ProductsController],
-  imports: [TypeOrmModule.forFeature([ProductsRepository, CategoriesRepository])],
-  providers: [ProductsService]
+	controllers: [ ProductsController ],
+	imports: [
+		TypeOrmModule.forFeature([ ProductsRepository, CategoriesRepository ]),
+		MulterModule.register({
+			storage: diskStorage({ filename: editFileName, destination: './uploads' }),
+			fileFilter: imageFileValidator
+		})
+	],
+	providers: [ ProductsService ]
 })
 export class ProductModule {}
