889276f3155e34cdc2caaba707bf2d8110d85b41
IAmThatG <gabbygabbylexy@gmail.com>
2019-11-17 12:22:10 +0100

    modified inventory update

--- src/models/request-dto/inventory-request-dto.ts
+++ src/models/request-dto/inventory-request-dto.ts
@@ -2,23 +2,23 @@ import { ApiModelProperty } from "@nestjs/swagger";
 import { IsNotEmpty } from "class-validator";
 
 export class InventoryRequestDto {
-    @ApiModelProperty({ required: true, type: Number, nullable: false })
+    @ApiModelProperty({ required: true, nullable: false })
     @IsNotEmpty()
     initialQuantity: number;
 
-    @ApiModelProperty({ required: true, type: Number, nullable: false })
+    @ApiModelProperty({ required: true, nullable: false })
     @IsNotEmpty()
     initialPrice: number;
 
-    @ApiModelProperty({ required: true, type: Number, nullable: false })
+    @ApiModelProperty({ required: true, nullable: false })
     @IsNotEmpty()
     currentQuantity: number;
 
-    @ApiModelProperty({ required: true, type: Number, nullable: false })
+    @ApiModelProperty({ required: true, nullable: false })
     @IsNotEmpty()
     currentPrice: number;
 
-    @ApiModelProperty({ required: true, type: Number, nullable: false })
+    @ApiModelProperty({ required: true, nullable: false })
     @IsNotEmpty()
     productId: number;
 }
