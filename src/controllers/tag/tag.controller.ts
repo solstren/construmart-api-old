4f449f7e51f4ef583d3410f890a5dcf521794dae
tmp10513 <tmp10513@firstbanknigeria.com>
2020-02-02 16:51:55 +0100

    implemented endpoint to get all tags

--- src/controllers/tag/tag.controller.ts
+++ src/controllers/tag/tag.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { BaseResponse } from './../../models/response-dto/base-response';
+import { AppConstants } from './../../utils/app-constants';
+import { TagsService } from './services/tags.service';
+import { Controller, Get, Query, ParseIntPipe } from '@nestjs/common';
+import { ApiOkResponse, ApiNotFoundResponse, ApiInternalServerErrorResponse } from '@nestjs/swagger';
 
 @Controller('tag')
-export class TagController {}
+export class TagController {
+    constructor(private _tagService: TagsService) { }
+
+    @ApiOkResponse({
+        description: AppConstants.SWAGGER_200_DESCRIPTION,
+        type: BaseResponse
+    })
+    @ApiNotFoundResponse({
+        description: AppConstants.SWAGGER_404_DESCRIPTION,
+        type: BaseResponse
+    })
+    @ApiInternalServerErrorResponse({
+        description: AppConstants.SWAGGER_500_DESCRIPTION
+    })
+    @Get()
+    async getAllTags(): Promise<BaseResponse> {
+        return await this._tagService.getAllTags();
+    }
+}
